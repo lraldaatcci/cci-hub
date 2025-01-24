60c3111cee9fac6eec0aa2cea479ab2dea12c362
Luis Fernando Ralda Estrada <lraldaatcci>
2025-01-23 23:02:48 -0600

    Refine Dockerfile, Astro config, and Hero component styling

--- apps/landing/astro.config.mjs
+++ apps/landing/astro.config.mjs
@@ -1,5 +1,6 @@
 import { defineConfig } from "astro/config";
 import node from "@astrojs/node";
+import viteConfig from "./vite.config";
 
 import tailwind from "@astrojs/tailwind";
 
@@ -9,4 +10,5 @@ export default defineConfig({
   output: "server",
   adapter: node({ mode: "standalone" }),
   integrations: [tailwind({ configFile: "./tailwind.config.mjs" })],
+  vite: viteConfig,
 });
